6ea7967711902819de8e2b1ac28f58e97e9abd22
Jean Saury <jeansar504@gmail.com>
2020-09-13 13:55:35 +0200

    add html header and footer in php

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+echo '
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <title>Delphine Jacquot-Crampes Coaching et formation</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+	  <link rel="stylesheet" href="css/w3.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <style>
+    h1{font-family: Raleway; sans-serif; color: white; font-size: 75px; padding-left: 100px; padding-top: 100px;}
+  </style>
+  <body>
+';
+
+?>
--- index.php
+++ index.php
@@ -1,33 +1,14 @@
-<!DOCTYPE html>
-<html lang="fr" dir="ltr">
-  <head>
-    <title>Delphine Jacquot-Crampes Coaching et formation</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-	<link rel="stylesheet" href="css/w3.css">
-    <link rel="stylesheet" href="css/style.css">
-  </head>
-  <style>
-    h1{font-family: Raleway; sans-serif; color: white; font-size: 75px; padding-left: 100px; padding-top: 100px;}
-  </style>
-  <body>
+<!--Header & Navbar -->
+<?php
+  require_once 'header.php';
+  require_once 'navbar.php';
+?>
+<div class="w3-display-container">
+  <img src="assets/docs/footages/Acceuil.jpg" alt="Image d'Acceuil"  style="width:100%">
+  <div class="w3-display-topleft"><h1>Delphine Jacquot-crampes<br> Coaching & Formation</h1></div>
+</div>
 
-  <!-- Navbar -->
-  <?php
-    require 'navbar.php';
-  ?>
-
-  <!-- Header -->
-  <header class="w3-display-container">
-    <img src="assets/docs/footages/Acceuil.jpg" alt="Image d'Acceuil"  style="width:100%">
-    <div class="w3-display-topleft"><h1>Delphine Jacquot<br> Coaching</h1></div>
-  </header>
-
-  <!-- Footer -->
-  <?php
-    require 'footer.php';
-  ?>
-  </body>
-</html>
+<!-- Footer -->
+<?php
+  require_once 'footer.php';
+?>
