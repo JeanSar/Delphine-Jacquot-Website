2540e581ddf85547ebf174c4ae5af09ab229b66a
JeanSar <jeansar504@gmail.com>
2020-09-16 10:31:10 +0200

    add links of footer's buttons

--- footer.php
+++ footer.php
@@ -2,11 +2,11 @@
 echo '
     <footer class="w3-center w3-black w3-padding-64">
       <div class="w3-large w3-section">
-        <i class="fa fa-facebook-official w3-hover-opacity"></i>
-        <i class="fa fa-instagram w3-hover-opacity"></i>
-        <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-        <i class="fa fa-twitter w3-hover-opacity"></i>
-        <i class="fa fa-linkedin w3-hover-opacity"></i>
+        <a href="https://fr-fr.facebook.com/"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
+        <a href="https://www.instagram.com/?hl=fr"><i class="fa fa-instagram w3-hover-opacity"></i></a>
+        <a href="https://www.pinterest.fr/"><i class="fa fa-pinterest-p w3-hover-opacity"></i></a>
+        <a href="https://twitter.com/?lang=fr"><i class="fa fa-twitter w3-hover-opacity"></i></a>
+        <a href="https://fr.linkedin.com/"><i class="fa fa-linkedin w3-hover-opacity"></i></a>
         </div>
     </footer>
   </body>
